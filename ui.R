4eb56c88fb5c3060d5ce455eb83d07afe00030d4
Nick Carchedi <nick.carchedi@gmail.com>
2016-10-20 19:18:57 -0400

    shinydashboard

--- ui.R
+++ ui.R
@@ -1,30 +1,30 @@
 library(shiny)
+library(shinydashboard)
 
-shinyUI(fluidPage(
-
-  titlePanel("Find the best fit line"),
-
-  sidebarLayout(
-    sidebarPanel(
-      sliderInput("n",
-                  "Number of points:",
-                  min = 2,
-                  max = 507,
-                  value = 20
-      ),
-      sliderInput("sl",
-                  "Slope of line:",
-                  min = -2,
-                  max = 5,
-                  value = 2,
-                  step = 0.25
-      ),
-      checkboxInput("show_best_fit", "Show best fit line"),
-      checkboxInput("show_residuals", "Show residuals")
+dashboardPage(
+  dashboardHeader(title = "Find the best fit line"),
+  dashboardSidebar(
+    sliderInput("n",
+                "Number of points:",
+                min = 2,
+                max = 507,
+                value = 20
     ),
-
-    mainPanel(
-      plotOutput("the_plot")
+    sliderInput("sl",
+                "Slope of line:",
+                min = -2,
+                max = 5,
+                value = 2,
+                step = 0.25
+    ),
+    checkboxInput("show_best_fit", "Show best fit line"),
+    checkboxInput("show_residuals", "Show residuals")
+  ),
+  dashboardBody(
+    fluidRow(
+      box(width = 6,
+        plotOutput("the_plot")
+      )
     )
   )
-))
+)
