925667a325b1a958f98d3f31fee4ea8f079833d5
Nick Carchedi <nick.carchedi@gmail.com>
2016-11-23 21:31:57 -0500

    bootstrap example working - but ui needs love

--- bootstrap/global.R
+++ bootstrap/global.R
@@ -9,7 +9,15 @@ summarize_flips <- function(flips) {
     )
 }
 
-set.seed(234)
-flips_orig <- flip_coin(10, p_heads = 0.65)
-flips_orig
-summarize_flips(flips_orig)
+save_summary <- function(this_summary) {
+  if(exists("all_summaries")) {
+    all_summaries <<- bind_rows(all_summaries, this_summary)
+  } else {
+    all_summaries <<- this_summary
+  }
+}
+
+# set.seed(234)
+# flips_orig <- flip_coin(10, p_heads = 0.65)
+# flips_orig
+# summarize_flips(flips_orig)
--- bootstrap/server.R
+++ bootstrap/server.R
@@ -4,9 +4,16 @@ library(dplyr)
 
 shinyServer(function(input, output) {
 
+  ###### INITIALIZE REACTIVE VALUES ######
+  vals <- reactiveValues(all_bootstrap_summaries = data_frame())
+
+  ###### ORIGINAL FLIPS + SUMMARY ######
   flips_orig <- reactive({
 
     input$flip_coin_button
+
+    # If coin is flipped, clear saved summaries
+    vals$all_bootstrap_summaries <- data_frame()
 
     isolate({
       n_flips <- input$n_flips
@@ -22,34 +29,54 @@ shinyServer(function(input, output) {
     )
   })
 
+  flips_orig_summary <- reactive({
+    summarize_flips(flips_orig())
+  })
+
+  ###### BOOTSTRAPPED FLIPS + SUMMARY ######
   flips_boot <- reactive({
     input$bootstrap_button
     dplyr::sample_frac(flips_orig(), replace = TRUE)
   })
 
-  output$bootstrap_dotplot <- renderPlot({
-    flips <- flips_boot()
-    flips_summary <- summarize_flips(flips)
+  flips_boot_summary <- reactive({
+    summarize_flips(flips_boot())
+  })
 
-    ggplot(flips_summary, aes(x = prop_heads)) +
+  ###### UPDATE SAVED BOOTSTRAP SUMMARIES ######
+  observeEvent(input$bootstrap_button, {
+
+    all_summaries <- vals$all_bootstrap_summaries
+    this_summary <- flips_boot_summary()
+
+    vals$all_bootstrap_summaries <- bind_rows(all_summaries, this_summary)
+  })
+
+  ###### DOTPLOT OF BOOTSTRAPPED PROPORTIONS ######
+  output$bootstrap_dotplot <- renderPlot({
+    all_summaries <- vals$all_bootstrap_summaries
+    validate(need(nrow(all_summaries) > 0, message = "Click 'Take Bootstrap Sample' to get started!"))
+
+    ggplot(all_summaries, aes(x = prop_heads)) +
       geom_dotplot(binwidth = 0.05) +
       theme(legend.position = "none") +
       xlim(c(0, 1))
   })
 
+  ###### PRINT SUMMARIES ######
   output$original_data <- renderPrint({
-    print(flips_orig())
+    flips_orig()
   })
 
   output$original_summary <- renderPrint({
-    summarize_flips(flips_orig())
+    flips_orig_summary()
   })
 
   output$bootstrap_data <- renderPrint({
-    print(flips_boot())
+    flips_boot()
   })
 
   output$bootstrap_summary <- renderPrint({
-    summarize_flips(flips_boot())
+    flips_boot_summary()
   })
 })
